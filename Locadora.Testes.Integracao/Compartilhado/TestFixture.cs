a9facf95ed7b9a3adad5826c5b2b69333eb54e1f
Victor R. Silva <victorr.silva005@gmail.com>
2025-11-29 14:23:46 -0300

    Testes de integração do módulo grupo veículo concluído

---
+++ Locadora.Testes.Integracao/Compartilhado/TestFixture.cs
@@ -0,0 +1,136 @@
+﻿using DotNet.Testcontainers.Containers;
+using FizzWare.NBuilder;
+using Locadora.Dominio.ModuloAluguel;
+using Locadora.Dominio.ModuloCliente;
+using Locadora.Dominio.ModuloCobranca;
+using Locadora.Dominio.ModuloCombustivel;
+using Locadora.Dominio.ModuloCondutor;
+using Locadora.Dominio.ModuloFuncionario;
+using Locadora.Dominio.ModuloGrupoVeiculo;
+using Locadora.Dominio.ModuloVeiculo;
+using Locadora.Infraestrutura.Compartilhado;
+using Locadora.Infraestrutura.ModuloAluguel;
+using Locadora.Infraestrutura.ModuloCliente;
+using Locadora.Infraestrutura.ModuloCobranca;
+using Locadora.Infraestrutura.ModuloCombustivel;
+using Locadora.Infraestrutura.ModuloCondutor;
+using Locadora.Infraestrutura.ModuloFuncionario;
+using Locadora.Infraestrutura.ModuloGrupoVeiculo;
+using Locadora.Infraestrutura.ModuloTaxa;
+using Locadora.Infraestrutura.ModuloVeiculo;
+using Microsoft.EntityFrameworkCore;
+using Testcontainers.PostgreSql;
+
+namespace Locadora.Testes.Integracao.Compartilhado;
+
+[TestClass]
+public abstract class TestFixture
+{
+    protected AppDbContext? appDbContext;
+
+    protected RepositorioGrupoVeiculoEmOrm? repositorioGrupoVeiculoEmOrm;
+    protected RepositorioCombustivelEmOrm? repositorioCombustivelEmOrm;
+    protected RepositorioVeiculoEmOrm? repositorioVeiculoEmOrm;
+    protected RepositorioFuncionarioEmOrm? repositorioFuncionarioEmOrm;
+    protected RepositorioClienteEmOrm? repositorioClienteEmOrm;
+    protected RepositorioAluguelEmOrm? repositorioAluguelEmOrm;
+    protected RepositorioCobrancaEmOrm? repositorioCobrancaEmOrm;
+    protected RepositorioCondutorEmOrm? repositorioCondutorEmOrm;
+    protected RepositorioTaxaEmOrm? repositorioTaxaEmOrm;
+
+    private static IDatabaseContainer? container;
+
+    [AssemblyInitialize]
+    public static async Task Setup(TestContext _)
+    {
+        container = new PostgreSqlBuilder()
+            .WithImage("postgres:16")
+            .WithName("locadora-testdb")
+            .WithDatabase("LocadoraTestDb")
+            .WithUsername("postgres")
+            .WithPassword("YourStrongPassword")
+            .WithCleanUp(true)
+            .Build();
+
+        await InicializarBancoDadosAsync(container);
+    }
+
+    [AssemblyCleanup]
+    public static async Task Teardown()
+    {
+        await EncerrarBancoDadosAsync();
+    }
+
+    [TestInitialize]
+    public void ConfigurarTestes()
+    {
+        if (container is null)
+            throw new ArgumentNullException("O Banco de dados não foi inicializado.");
+
+        appDbContext = AppDbContextFactory.CriarDbContext(container.GetConnectionString());
+
+        ConfigurarTabelas(appDbContext);
+
+        repositorioGrupoVeiculoEmOrm = new RepositorioGrupoVeiculoEmOrm(appDbContext);
+        repositorioCombustivelEmOrm = new RepositorioCombustivelEmOrm(appDbContext);
+        repositorioVeiculoEmOrm = new RepositorioVeiculoEmOrm(appDbContext);
+        repositorioCondutorEmOrm = new RepositorioCondutorEmOrm(appDbContext);
+        repositorioClienteEmOrm = new RepositorioClienteEmOrm(appDbContext);
+        repositorioFuncionarioEmOrm = new RepositorioFuncionarioEmOrm(appDbContext);
+        repositorioCobrancaEmOrm = new RepositorioCobrancaEmOrm(appDbContext);
+        repositorioAluguelEmOrm = new RepositorioAluguelEmOrm(appDbContext);
+
+        BuilderSetup.SetCreatePersistenceMethod<GrupoVeiculo>(async grupoVeiculo => await repositorioGrupoVeiculoEmOrm.CadastrarAsync(grupoVeiculo));
+        BuilderSetup.SetCreatePersistenceMethod<IList<GrupoVeiculo>>(async gruposVeiculos => await repositorioGrupoVeiculoEmOrm.CadastrarEntidades(gruposVeiculos));
+
+        BuilderSetup.SetCreatePersistenceMethod<Combustivel>(async combustivel => await repositorioCombustivelEmOrm.CadastrarAsync(combustivel));
+        BuilderSetup.SetCreatePersistenceMethod<IList<Combustivel>>(async combustiveis => await repositorioCombustivelEmOrm.CadastrarEntidades(combustiveis));
+
+        BuilderSetup.SetCreatePersistenceMethod<Veiculo>(async veiculo => await repositorioVeiculoEmOrm.CadastrarAsync(veiculo));
+        BuilderSetup.SetCreatePersistenceMethod<IList<Veiculo>>(async veiculos => await repositorioVeiculoEmOrm.CadastrarEntidades(veiculos));
+
+        BuilderSetup.SetCreatePersistenceMethod<Condutor>(async condutor => await repositorioCondutorEmOrm.CadastrarAsync(condutor));
+        BuilderSetup.SetCreatePersistenceMethod<IList<Condutor>>(async condutores => await repositorioCondutorEmOrm.CadastrarEntidades(condutores));
+
+        BuilderSetup.SetCreatePersistenceMethod<Cliente>(async cliente => await repositorioClienteEmOrm.CadastrarAsync(cliente));
+        BuilderSetup.SetCreatePersistenceMethod<IList<Cliente>>(async clientes => await repositorioClienteEmOrm.CadastrarEntidades(clientes));
+
+        BuilderSetup.SetCreatePersistenceMethod<Funcionario>(async funcionario => await repositorioFuncionarioEmOrm.CadastrarAsync(funcionario));
+        BuilderSetup.SetCreatePersistenceMethod<IList<Funcionario>>(async funcionarios => await repositorioFuncionarioEmOrm.CadastrarEntidades(funcionarios));
+
+        BuilderSetup.SetCreatePersistenceMethod<Cobranca>(async cobranca => await repositorioCobrancaEmOrm.CadastrarAsync(cobranca));
+        BuilderSetup.SetCreatePersistenceMethod<IList<Cobranca>>(async cobrancas => await repositorioCobrancaEmOrm.CadastrarEntidades(cobrancas));
+
+        BuilderSetup.SetCreatePersistenceMethod<Aluguel>(async aluguel => await repositorioAluguelEmOrm.CadastrarAsync(aluguel));
+        BuilderSetup.SetCreatePersistenceMethod<IList<Aluguel>>(async alugueis => await repositorioAluguelEmOrm.CadastrarEntidades(alugueis));
+    }
+
+    private static void ConfigurarTabelas(AppDbContext dbContext)
+    {
+        dbContext.Database.EnsureCreated();
+
+        dbContext.aluguel.RemoveRange(dbContext.aluguel);
+        dbContext.cobrancas.RemoveRange(dbContext.cobrancas);
+        dbContext.funcionarios.RemoveRange(dbContext.funcionarios);
+        dbContext.clientes.RemoveRange(dbContext.clientes);
+        dbContext.condutores.RemoveRange(dbContext.condutores);
+        dbContext.veiculos.RemoveRange(dbContext.veiculos);
+        dbContext.combustivels.RemoveRange(dbContext.combustivels);
+        dbContext.grupoVeiculos.RemoveRange(dbContext.grupoVeiculos);
+
+        dbContext.SaveChanges();
+    }
+    private static async Task InicializarBancoDadosAsync(IDatabaseContainer container)
+    {
+        await container.StartAsync();
+    }
+
+    private static async Task EncerrarBancoDadosAsync()
+    {
+        if (container is null)
+            throw new ArgumentNullException("O Banco de dados não foi inicializado.");
+
+        await container.StopAsync();
+        await container.DisposeAsync();
+    }
+}
---
+++ Locadora.Testes.Integracao/ModuloGrupoVeiculo/RepositorioGrupoVeiculoEmOrmTests.cs
@@ -0,0 +1,118 @@
+﻿using Locadora.Dominio.ModuloGrupoVeiculo;
+using Locadora.Testes.Integracao.Compartilhado;
+
+namespace Locadora.Testes.Integracao.ModuloGrupoVeiculo;
+
+[TestClass]
+[TestCategory("Testes de Integração de grupo veículo")]
+public sealed class RepositorioGrupoVeiculoEmOrmTests : TestFixture
+{
+    [TestMethod]
+    public async Task Deve_Cadastrar_GrupoVeiculo_Corretamente()
+    {
+        // Arrange
+        var grupoVeiculo = new GrupoVeiculo("SUV");
+
+        // Act
+        await repositorioGrupoVeiculoEmOrm?.CadastrarAsync(grupoVeiculo)!;
+        appDbContext?.SaveChanges();
+
+        // Assert
+        var registroSelecionado = repositorioGrupoVeiculoEmOrm?.SelecionarRegistroPorIdAsync(grupoVeiculo.Id).Result;
+
+        Assert.AreEqual(grupoVeiculo, registroSelecionado);
+    }
+
+    [TestMethod]
+    public async Task Deve_Editar_GrupoVeiculo_Corretamente()
+    {
+        // Arrange
+        var grupoVeiculo = new GrupoVeiculo("SUV");
+
+        await repositorioGrupoVeiculoEmOrm?.CadastrarAsync(grupoVeiculo)!;
+        appDbContext?.SaveChanges();
+
+        var grupoVeiculoEditado = new GrupoVeiculo("Coupe");
+
+        // Act
+        var conseguiuEditar = await repositorioGrupoVeiculoEmOrm?.EditarAsync(grupoVeiculo.Id, grupoVeiculoEditado)!;
+        appDbContext?.SaveChanges();
+
+        // Assert
+        var registroSelecionado = repositorioGrupoVeiculoEmOrm?.SelecionarRegistroPorIdAsync(grupoVeiculo.Id).Result;
+        Assert.AreEqual(grupoVeiculo, registroSelecionado);
+        Assert.IsTrue(conseguiuEditar);
+    }
+
+    [TestMethod]
+    public async Task Deve_Excluir_GrupoVeiculo_Corretamente()
+    {
+        // Arrange
+        var grupoVeiculo = new GrupoVeiculo("SUV");
+
+        await repositorioGrupoVeiculoEmOrm?.CadastrarAsync(grupoVeiculo)!;
+        appDbContext?.SaveChanges();
+
+        // Act
+        var conseguiuExcluir = await repositorioGrupoVeiculoEmOrm?.ExcluirAsync(grupoVeiculo.Id)!;
+        appDbContext?.SaveChanges();
+
+        // Assert
+        Assert.IsTrue(conseguiuExcluir);
+    }
+
+    [TestMethod]
+    public async Task Deve_Selecionar_Todos_GrupoVeiculos_Corretamente()
+    {
+        // Arrange
+        var grupoVeiculo1 = new GrupoVeiculo("SUV");
+        var grupoVeiculo2 = new GrupoVeiculo("Hatch");
+        var grupoVeiculo3 = new GrupoVeiculo("Sedan");
+
+        await repositorioGrupoVeiculoEmOrm?.CadastrarEntidades(new List<GrupoVeiculo> { grupoVeiculo1, grupoVeiculo2, grupoVeiculo3 })!;
+        appDbContext?.SaveChanges();
+
+        // Act
+        var gruposVeiculo = await repositorioGrupoVeiculoEmOrm?.SelecionarRegistrosAsync()!;
+
+        // Assert
+        Assert.AreEqual(3, gruposVeiculo.Count);
+        CollectionAssert.Contains(gruposVeiculo, grupoVeiculo1);
+        CollectionAssert.Contains(gruposVeiculo, grupoVeiculo2);
+        CollectionAssert.Contains(gruposVeiculo, grupoVeiculo3);
+    }
+
+    [TestMethod]
+    public async Task Deve_Selecionar_GrupoVeiculo_Com_Quantidade_Especifica_Corretamente()
+    {
+        // Arrange
+        var grupoVeiculo1 = new GrupoVeiculo("SUV");
+        var grupoVeiculo2 = new GrupoVeiculo("Hatch");
+        var grupoVeiculo3 = new GrupoVeiculo("Sedan");
+
+        await repositorioGrupoVeiculoEmOrm?.CadastrarEntidades(new List<GrupoVeiculo> { grupoVeiculo1, grupoVeiculo2, grupoVeiculo3 })!;
+        appDbContext?.SaveChanges();
+
+        // Act
+        var gruposVeiculo = await repositorioGrupoVeiculoEmOrm?.SelecionarRegistrosAsync(2)!;
+
+        // Assert
+        Assert.AreEqual(2, gruposVeiculo.Count);
+    }
+
+    [TestMethod]
+    public async Task Deve_Selecionar_GrupoVeiculo_Por_Id_Corretamente()
+    {
+        // Arrange
+        var grupoVeiculo = new GrupoVeiculo("SUV");
+
+        await repositorioGrupoVeiculoEmOrm?.CadastrarAsync(grupoVeiculo)!;
+        appDbContext?.SaveChanges();
+
+        // Act
+        var grupoVeiculoEncontrado = await repositorioGrupoVeiculoEmOrm?.SelecionarRegistroPorIdAsync(grupoVeiculo.Id)!;
+
+        // Assert
+        Assert.AreEqual(grupoVeiculo, grupoVeiculoEncontrado);
+    }
+}
